4b4d49e717c8dd4028b00f378fd667b9f2b89b4c
smairs <s.mairs@eaobservatory.org>
2021-01-21 17:46:30 -1000

    Updated makemap script to ensure 450 microns only runs once and uses proper pointing offsets

--- create_makemap_script.py
+++ create_makemap_script.py
@@ -22,6 +22,8 @@ def make_pcor(tablefile):
                 pcorfile.write('10000000 '+str(eachdx)+' '+str(eachdy))
                 pcorfile.close()
                 print('POINTING FOR '+eachdate.decode('utf-8')+': '+str(eachdx)+', '+str(eachdy))
+    os.system('mkdir pointing_files_posterity')
+    os.system('cp *_pcor.txt pointing_files_posterity')
 
 def makemap_infiles(region,wave):
 
